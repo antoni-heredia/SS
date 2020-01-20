9eaf226fd7788d47dae31d39511d3b283e616114
Antonio Jesus Heredia Castillo <a.heredia.castillo@gmail.com>
2020-01-20 12:20:25 +0100

    terminado ejercicio 1

--- eje1/codigo/main.cpp
+++ eje1/codigo/main.cpp
@@ -22,15 +22,17 @@ int generademanda()
         x = a + sqrt((b - a) * (c - a) * u);
     else
         x = b - sqrt((b - a) * (b - c) * (1 - u));
-    return (int)x; //se convierte a entero porque es la demanda de huevos de pascua
+    return (int)x; 
 }
 
 int main(int argc, char *argv[])
 {
+    // declaro los peso de cada huevo, los el precio del pedido en cada epoca y el precio de venta
     double peso_huevo = 0.25;
     double precio_kilo_a = 1;
     double precio_kilo_d = 1.5;
     double precio_venta = 0.6;
+    // Donde guardare el numero de repeticiones que pone el usuario
     int num_repeticiones;
     if (argc != 2)
     {
@@ -47,31 +49,36 @@ int main(int argc, char *argv[])
     double ganancia_ventas,costes, beneficio;
     double mejor_ganancia = 0;
     int mejor_suministro;
-
+    // Repetimos desde el valor minimo, hasta el maximo del intervalo triangular
     for (int i = a; i < b; i++)
     {
+        //Variable para guardar la media
         double media = 0.0;
+        //Repetimos tantas veces como indique el usuario
         for (int x = 0; x < num_repeticiones; x++)
         {
+            //Generamos una demanda 
             demanda = generademanda();
             ganancia_ventas = (double)demanda * precio_venta;
+            //Si los pedidos son menores a los que podemos producir
             if(i >= demanda){
-                costes = (i/4)*precio_kilo_a;
-            }else{
-                costes = (i/4)*precio_kilo_a+((demanda - i)/4)*precio_kilo_d;
+                costes = (i*peso_huevo)*precio_kilo_a;
+            }else{ // SI los pedidos es mayores a loo que podemos realizar, tenemos que pedir de nuevo
+                costes = (i*peso_huevo)*precio_kilo_a+((demanda - i)*peso_huevo)*precio_kilo_d;
             }
+            //Calculamos el beneficio restando las ventas menos los costes
             beneficio = ganancia_ventas - costes;
 
             media += beneficio;
         }
+        //Calculamos la media y guardamos la mejor
         media /= num_repeticiones;
-        cout << "Media: " << media << endl;
         if(media > mejor_ganancia){
-            mejor_suministro = i/4;
+            mejor_suministro = i*peso_huevo;
             mejor_ganancia = media;
         }
     }
-
+    //Mostramos los dsatos
     cout << "La mejor ganancia ha sido: " << mejor_ganancia << endl;
     cout << "El mejor pedido en agosto es: " << mejor_suministro << endl;
 
